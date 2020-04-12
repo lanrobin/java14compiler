c749a522e193cffb8d9384c199f4dedea5238e97
Robin Lan <rolan@microsoft.com>
2020-04-12 13:08:12 +0800

    Add inner class to debug.

--- test/icu/mianshi/openjdk/compiler/MyMath.java
+++ test/icu/mianshi/openjdk/compiler/MyMath.java
@@ -1,8 +1,29 @@
 package icu.mianshi.openjdk.compiler;
-
+import static java.lang.System.out;
 public class MyMath {
 	public int add(int a, int b) {
 		int c = a + b;
 		return c;
+	}
+	
+	private interface Mathable {
+		void calculate(Number n);
+	}
+	
+	private static class InnerMath implements Mathable {
+		@Override
+		public void calculate(Number n) {
+			out.println("Number:" + n);
+		}
+	}
+}
+
+class NonPublicClass{
+	public String hello() {
+		return "hello";
+	}
+	
+	public static interface Markable<T> {
+		void mark(T t);
 	}
 }
